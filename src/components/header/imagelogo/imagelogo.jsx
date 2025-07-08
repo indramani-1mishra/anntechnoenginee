c05f4ec05679f0cb6c85e8d255ed35b942aff896
RAJ <rajmishra.ydd@gmail.com>
2025-07-08 08:50:07 +0530

    updated the brand logo

--- src/components/header/imagelogo/imagelogo.jsx
+++ src/components/header/imagelogo/imagelogo.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import './image.css'
+import React from 'react';
+import './image.css';
 
 export default function Imagelogo() {
-    const img ='https://www.anntechno.in/wp-content/uploads/2024/04/logo_2551252_7064038.png'
+  const img ='https://5.imimg.com/data5/SELLER/Logo/2025/6/519657378/CO/VL/BQ/108774879/logo-with-india-map-90x90.png';
   return (
     <div className='image-container'>
-      <img src={img} className='imgs' />
+      <img src={img} className='imgs' alt='imagelogo' />
     </div>
-  )
+  );
 }
