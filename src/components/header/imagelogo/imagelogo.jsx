d4105abae94fb71d433b01a767fc6c759aaf604d
RAJ <rajmishra.ydd@gmail.com>
2025-07-18 07:07:54 +0530

    updated header

--- src/components/header/imagelogo/imagelogo.jsx
+++ src/components/header/imagelogo/imagelogo.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './image.css';
+import img from '/imagelogo.jpg';
 
 export default function Imagelogo() {
-  const img ='https://5.imimg.com/data5/SELLER/Logo/2025/6/519657378/CO/VL/BQ/108774879/logo-with-india-map-90x90.png';
+  
   return (
     <div className='image-container'>
       <img src={img} className='imgs' alt='imagelogo' />
